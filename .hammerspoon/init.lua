3100c82df9e2fe5a4660a03431ef1506ee1e3164
swimmingone <dev.swimmingone@gmail.com>
2023-10-31 19:53:18 +0900

    add shorcut for arc browser

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,30 +4,35 @@ require('modules.convert_new_line_to_comma')
 
 hs.hotkey.bind({'option', 'cmd'}, 'r',  hs.reload)
 
-hs.hotkey.bind({'shift', 'option'}, 'N', function()
-    hs.application.launchOrFocus('Notes')
+hs.hotkey.bind({'shift', 'option'}, 'A', function()
+    hs.application.launchOrFocus('Arc')
 end)
 
 hs.hotkey.bind({'shift', 'option'}, 'C', function()
     hs.application.launchOrFocus('Google Chrome')
 end)
 
-hs.hotkey.bind({'shift', 'option'}, 'T', function()
-    hs.application.launchOrFocus('iTerm')
-end)
-
-hs.hotkey.bind({'shift', 'option'}, 'S', function()
-    hs.application.launchOrFocus('Slack')
-end)
-
 hs.hotkey.bind({'shift', 'option'}, 'K', function()
     hs.application.launchOrFocus('KakaoTalk')
+end)
+
+hs.hotkey.bind({'shift', 'option'}, 'N', function()
+    hs.application.launchOrFocus('Notes')
 end)
 
 hs.hotkey.bind({'shift', 'option'}, 'O', function()
     hs.application.launchOrFocus('Obsidian')
 end)
 
+hs.hotkey.bind({'shift', 'option'}, 'S', function()
+    hs.application.launchOrFocus('Slack')
+end)
+
+hs.hotkey.bind({'shift', 'option'}, 'T', function()
+    hs.application.launchOrFocus('iTerm')
+end)
+
 hs.hotkey.bind({'shift', 'option'}, 'W', function()
     hs.application.launchOrFocus('WorkFlowy')
 end)
+
